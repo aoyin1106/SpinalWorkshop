cb74eb91c3f180d6f49cce01dde7489eeab35de9
aoyin <aoyin@umich.edu>
2022-05-18 18:45:42 -0400

    APbPwm - notice resize when assign

--- src/main/scala/workshop/pwm/ApbPwm.scala
+++ src/main/scala/workshop/pwm/ApbPwm.scala
@@ -11,11 +11,17 @@ case class ApbConfig(addressWidth : Int,
 
 //APB interface definition
 case class Apb(config: ApbConfig) extends Bundle with IMasterSlave {
-  //TODO define APB signals
+  val PSEL       = Bits(config.selWidth bits)
+  val PENABLE    = Bool()
+  val PADDR      = UInt(config.addressWidth bit)
+  val PWRITE     = Bool()
+  val PWDATA     = Bits(config.dataWidth bit)
+  val PRDATA     = Bits(config.dataWidth bit)
+  val PREADY     = Bool()
 
   override def asMaster(): Unit = {
-    //TODO define direction of each signal in a master mode
-
+    out(PADDR,PSEL,PENABLE,PWRITE,PWDATA)
+    in(PREADY,PRDATA)
   }
 }
 
@@ -24,15 +30,50 @@ case class ApbPwm(apbConfig: ApbConfig,timerWidth : Int) extends Component{
   require(apbConfig.selWidth == 1)
 
   val io = new Bundle{
-    val apb = ??? //TODO
-    val pwm = ??? //TODO
+    val apb = slave(Apb(apbConfig))
+    val pwm = out (Reg(Bool()) init(False))
   }
 
   val logic = new Area {
-    //TODO define the PWM logic
+
+    val enable = Reg(Bool()) init(False)
+    val timer = Reg(UInt(timerWidth bits)) init(0)
+    val dutyCycle = Reg(UInt(timerWidth bits)) init(0)
+
+    when(enable){
+      timer := timer + 1
+    }
+
+    when(timer === 0){
+      io.pwm := True
+    }
+    when(timer === dutyCycle){
+      io.pwm := False
+    }
   }
   
   val control = new Area{
-    //TODO define the APB slave logic that will make PWM's registers writable/readable
+  
+    io.apb.PREADY := True
+    io.apb.PRDATA := 0
+    switch(io.apb.PADDR){
+      is(0){
+        io.apb.PRDATA := logic.enable.asBits.resized
+      }
+      is(4){
+        io.apb.PRDATA := logic.dutyCycle.asBits.resized
+      }
+    }
+
+    when(io.apb.PENABLE && io.apb.PSEL(0) && io.apb.PWRITE){
+      switch(io.apb.PADDR){
+      is(0){
+        logic.enable := io.apb.PWDATA(0)
+      }
+      is(4){
+        logic.dutyCycle := io.apb.PWDATA.asUInt.resized
+      }
+    }
+    }
   }
 }
