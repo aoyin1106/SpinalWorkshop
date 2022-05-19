c0e609b79a25669c4c493304a6dc1033469f9b59
aoyin <aoyin@umich.edu>
2022-05-18 20:16:20 -0400

    Timer - busSlaveFac must use - busCtrl created regs also should be initiated

--- src/main/scala/workshop/timer/Timer.scala
+++ src/main/scala/workshop/timer/Timer.scala
@@ -15,9 +15,27 @@ case class Timer(width : Int) extends Component {
     val value = out UInt (width bits)
   }
 
-  //TODO phase 1
+  val timer = Reg(UInt(width bits)) init(0)
+  when(io.tick && timer < io.limit){
+    timer := timer + 1
+  }
+  when(io.clear){
+    timer := 0
+  }
+
+  io.full  := timer === io.limit
+  io.value := timer
 
   def driveFrom(busCtrl : BusSlaveFactory,baseAddress : BigInt)(ticks : Seq[Bool],clears : Seq[Bool]) = new Area {
-    //TODO phase 2
+    val ticksEnable  = busCtrl.createReadAndWrite(dataType = Bits(ticks.length  bits), address = baseAddress, bitOffset =  0)  init(0)
+    val clearsEnable = busCtrl.createReadAndWrite(dataType = Bits(clears.length bits), address = baseAddress, bitOffset =  16) init(0)
+
+    busCtrl.driveAndRead(io.limit, address = baseAddress + 4, bitOffset = 0)
+    busCtrl.read(io.value, address = baseAddress + 8, bitOffset = 0)
+
+    val busWriteClear = busCtrl.isWriting(address = baseAddress + 8) || busCtrl.isWriting(address = baseAddress + 4)
+
+    io.tick := (ticksEnable & ticks.asBits()).orR
+    io.clear:= (clearsEnable & clears.asBits()).orR || busWriteClear
   }
 }
